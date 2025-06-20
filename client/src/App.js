5f91d2045eec17b44e482add7b1cc938bfd67b0b
mokers2025 <mokewithrs@gmail.com>
2025-06-20 15:56:06 +0200

    💫 Clean React frontend for PublicFlix

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>🎬 Welcome to PublicFlix</h1>
+      <p>This is your frontend. Connected to backend soon.</p>
+    </div>
+  );
+}
+
+export default App;
